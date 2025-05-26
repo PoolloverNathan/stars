0f62f57836161bf56ac0e74c0b79f11b2d37a78e
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-05-26 17:43:56 -0400

    nixGL

--- shell.nix
+++ shell.nix
@@ -1,9 +1,13 @@
-{ pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/fe51d34885f7b5e3e7b59572796e1bcb427eccb1.tar.gz") {} }:
+{
+  pkgs ? import (fetchTarball "https://github.com/nixos/nixpkgs/archive/fe51d34885f7b5e3e7b59572796e1bcb427eccb1.tar.gz") {},
+  nixGL ? import (fetchTarball "https://github.com/nix-community/nixGL/archive/310f8e49a149e4c9ea52f1adf70cdc768ec53f8a.tar.gz") { inherit pkgs; },
+}:
 
 pkgs.mkShell {
   buildInputs = [
     pkgs.gradle_8
     pkgs.zulu21
     pkgs.bashInteractive
+    nixGL.auto.nixGLDefault
   ];
 }
