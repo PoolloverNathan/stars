c33d472fcd800ebbaff8ba23416fac1cc662ef49
PoolloverNathan <24194027+PoolloverNathan@users.noreply.github.com>
2025-06-17 11:55:28 -0400

    fix: make slot locking respect event

--- src/client/java/org/eu/net/pool/fabric/cots/mixin/client/SlotMixin.java
+++ src/client/java/org/eu/net/pool/fabric/cots/mixin/client/SlotMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
-import org.eu.net.pool.fabric.cots.NoInventoryCurse;
+import org.eu.net.pool.fabric.cots.InventorySlotAccess;
 import org.eu.net.pool.fabric.cots.StarsKt;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,36 +15,28 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(Slot.class)
-public class SlotMixin {
+public abstract class SlotMixin {
     @Shadow @Final public Inventory inventory;
     @Shadow @Final private int index;
+    @Shadow public abstract ItemStack getStack();
 
     @Unique
-    private int stars$getEffectiveSettlerLevel() {
-        return inventory instanceof PlayerInventory pi ? StarsKt.effectiveLevel(pi.player, NoInventoryCurse.INSTANCE) : 0;
-    }
-
-    @Unique
-    private boolean stars$doesSettlerAllowAccess() {
-        return !switch (stars$getEffectiveSettlerLevel()) {
-            case 0 -> false;
-            case 1 -> index >= 1 && index < 36;
-            default -> index >= 0 && index < 36;
-        };
+    private InventorySlotAccess stars$accessType() {
+        return inventory instanceof PlayerInventory pi ? StarsKt.getInventorySlotAccessEvent().invoker().invoke(getStack(), pi.player, index) : InventorySlotAccess.ALLOW;
     }
 
     @WrapMethod(method = "isEnabled")
     boolean isEnabled(Operation<Boolean> orig) {
-        return stars$doesSettlerAllowAccess() && orig.call();
+        return stars$accessType() != InventorySlotAccess.LOCK_AND_DROP && orig.call();
     }
 
     @WrapMethod(method = "canInsert")
     boolean canInsert(ItemStack stack, Operation<Boolean> orig) {
-        return stars$doesSettlerAllowAccess() && orig.call(stack);
+        return stars$accessType() == InventorySlotAccess.ALLOW && orig.call(stack);
     }
 
     @WrapMethod(method = {"canTakeItems", "canTakePartial"})
     boolean canTakeItems(PlayerEntity player, Operation<Boolean> orig) {
-        return stars$doesSettlerAllowAccess() && orig.call(player);
+        return stars$accessType() == InventorySlotAccess.ALLOW && orig.call(player);
     }
 }
